aa3c073e0282a6c05e42e472ea75bd42203252f1
jerguerrero <jguerrero@student.unimelb.edu.au>
2018-10-07 16:08:04 +1100

    Added laptop weapon and camera control for the cube character

---
+++ Comp30019Proj2/Assets/Scripts/ThrowWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowWeapon : MonoBehaviour {
+
+    GameObject prefab;
+	// Use this for initialization
+	void Start () {
+        prefab = Resources.Load("laptop") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)){
+            GameObject laptop = Instantiate(prefab) as GameObject;
+            laptop.transform.position = transform.position + Camera.main.transform.forward * 2;
+            Rigidbody rigidbody = laptop.GetComponent<Rigidbody>();
+            rigidbody.velocity = Camera.main.transform.forward * 40;
+        }
+	}
+}
